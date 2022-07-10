00bc8ba00d85c78aac31dac69520a8d85e6d499c
you@example.com <you@example.com>
2022-07-10 20:31:46 +0530

    update madhu url @changename

--- pages/madhu/boardingpass.js
+++ pages/madhu/boardingpass.js
@@ -58,7 +58,7 @@ function BoardingPassMadhu() {
 
                                         <div className='flex justify-between'>
                                             <div className='mt-[50px] ml-[30px] mb-[40px]'>
-                                                <QRCode value="https://flight-bag-tag-generator.vercel.app/madhu/form2" size="140" renderAs="canvas" />
+                                                <QRCode value="https://flight-bag-tag-generator-git-qr-x1ucifer.vercel.app/madhu/form2" size="140" renderAs="canvas" />
                                             </div>
 
                                             <div className='mt-[60px]'>
@@ -132,7 +132,7 @@ function BoardingPassMadhu() {
 
                                     <div className='flex justify-between'>
                                         <div className='mt-[10px] ml-[20px] mb-[40px]'>
-                                            <QRCode value="https://flight-bag-tag-generator.vercel.app/madhu/form2" size="100" renderAs="canvas" />
+                                            <QRCode value="https://flight-bag-tag-generator-git-qr-x1ucifer.vercel.app/madhu/form2" size="100" renderAs="canvas" />
                                         </div>
 
                                         <div className='mt-[20px] mr-[50px]'>
